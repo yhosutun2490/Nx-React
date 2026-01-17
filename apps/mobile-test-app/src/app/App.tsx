57bfacd12057d9c5699520e3745fa8385f13997f
yhosutun2491 <yhosutun2491@gmail.com>
2026-01-17 19:59:26 +0800

    chore: app react-test-render 降版

--- apps/mobile-test-app/src/app/App.tsx
+++ apps/mobile-test-app/src/app/App.tsx
@@ -1,66 +1,80 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
 import { useRef, useState } from 'react';
-import { ScrollView ,View, Text, StatusBar, StyleSheet, Image } from 'react-native';
-import { text } from 'stream/consumers';
+import {
+  ScrollView,
+  View,
+  Text,
+  TextInput,
+  Button,
+  StatusBar,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
 const App = () => {
-  const [whatsNextYCoord, setWhatsNextYCoord] = useState<number>(0);
+  const [text, setText] = useState<string>('');
+  const [goalsList, setGoalsList] = useState<string[]>([]);
+  const handleInputChange = (enteredText: string) => {
+    setText(enteredText);
+  };
+  const handleAddGoal = () => {
+    setGoalsList((currentGoals) => [...currentGoals, text]);
+    setText('');
+  };
 
   return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={{ gap:10, padding: 20 , justifyContent: 'center', alignItems: 'center'}}
-    >
-      <StatusBar barStyle="dark-content" />
-      <Text testID="header" style={styles.text}>Hello My First App</Text>
-      <Image
-        source={{ uri: 'https://picsum.photos/300' }}
-        style={{ width: 300, height: 300 }}
-      />
-      <Text>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32. The standard chunk of Lorem Ipsum used since the 1500s is
-        reproduced below for those interested. Sections 1.10.32 and 1.10.33 from
-        "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their
-        exact original form, accompanied by English versions from the 1914
-        translation by H. Rackham.
-      </Text>
-      <Text>
-        Where can I get some? There are many variations of passages of Lorem
-        Ipsum available, but the majority have suffered alteration in some form,
-        by injected humour, or randomised words which don't look even slightly
-        believable. If you are going to use a passage of Lorem Ipsum, you need
-        to be sure there isn't anything embarrassing hidden in the middle of
-        text. All the Lorem Ipsum generators on the Internet tend to repeat
-        predefined chunks as necessary, making this the first true generator on
-        the Internet. It uses a dictionary of over 200 Latin words, combined
-        with a handful of model sentence structures, to generate Lorem Ipsum
-        which looks reasonable. The generated Lorem Ipsum is therefore always
-        free from repetition, injected humour, or non-characteristic words etc.
-      </Text>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="add your goal"
+          style={styles.input}
+          onChangeText={handleInputChange}
+          value={text}
+        ></TextInput>
+        <Button title="Add Goal" onPress={handleAddGoal}></Button>
+      </View>
+      <View style={styles.goalsContainer}>
+        <Text>Goals Lists</Text>
+        <ScrollView>
+          {goalsList.map((goal, index) => (
+            <View
+              key={index}
+              style={{ marginVertical: 8, padding: 8, backgroundColor: '#ccc' }}
+            >
+              <Text>{goal}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
+    paddingHorizontal: 20,
     flex: 1,
-    backgroundColor: 'plum',
   },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    color: 'white',
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderBottomColor: '#cccccc',
+    borderBottomWidth: 1,
+    paddingBottom: 24,
+    flex: 1,
+  },
+  input: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+    padding: 10,
+    flex: 1,
+    marginRight: 16,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
+
 export default App;
